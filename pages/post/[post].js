a7d248aa46f286d53d32228f3265f38b6dab30e6
firmnardians <firmnardians@gmail.com>
2021-10-19 10:12:12 +0700

    first

---
+++ pages/post/[post].js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+
+function MetaPost({ title, description }) {
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name='description' content={`${description} `} />
+
+			<meta property='og:title' content={`${title} - ${description} - Example.com`} />
+			<meta property='og:description' content={`${title} - ${description} - Example.com`} />
+
+			<meta name='twitter:title' content={`${title} - ${description} - Example.com`} />
+			<meta name='twitter:description' content={`${title} - ${description} - Example.com`} />
+		</Head>
+	);
+}
+
+export default function Post({ data }) {
+	const { title, body, id } = data;
+	return (
+		<div>
+			<MetaPost title={title} description={body} />
+			<p>Id: {id}</p>
+			<p>Title: {title}</p>
+			<p>Description: {body}</p>
+		</div>
+	);
+}
+
+// Get Meta
+Post.getInitialProps = async ({ query }) => {
+	try {
+		const { post } = query; // get dynamic requests
+		const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}posts/${post}`;
+
+		const res = await fetch(url);
+		const json = await res.json();
+		return { data: json };
+	} catch (e) {
+		return { data: null };
+	}
+};
